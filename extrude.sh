2920432bd014c783e1bb479974da092ad7680ef1
Ankang Gao <agao@blackfriars.ae.ic.ac.uk>
2020-07-07 18:26:18 +0100

    modify extrude

--- extrude.sh
+++ extrude.sh
@@ -3,4 +3,4 @@ NekMesh -m extrude:layers=1 inFoil.xml      3Din.xml
 NekMesh -m extrude:layers=2 wakeRegion.xml  3Dwake.xml
 NekMesh 3Din.xml 3DinUcomp.xml:xml:uncompress
 NekMesh 3Dout.xml 3DoutUcomp.xml:xml:uncompress
-NekMesh 3Dwake.xml 3Dwake.xml:xml:uncompress
+NekMesh 3Dwake.xml 3DwakeUcomp.xml:xml:uncompress
