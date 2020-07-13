25fd070a4ff37a0513a6a6c450c763b5b9e337b8
ankang1 <ankang1@v.systems>
2020-07-13 12:34:16 +0100

    increase N2

--- extrude.sh
+++ extrude.sh
@@ -1,4 +1,4 @@
-NekMesh -m extrude:layers=30 outerRegion.xml 3Dout.xml
-NekMesh -m extrude:layers=5 inFoil.xml 3Din.xml
+NekMesh -m extrude:layers=32 outerRegion.xml 3Dout.xml
+NekMesh -m extrude:layers=7 inFoil.xml 3Din.xml
 NekMesh 3Din.xml 3DinUcomp.xml:xml:uncompress
 NekMesh 3Dout.xml 3DoutUcomp.xml:xml:uncompress
