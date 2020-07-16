2216c22f2d3f3fde892ca5798e4c1a7a3639948e
Ankang Gao <agao@blackfriars.ae.ic.ac.uk>
2020-07-16 16:35:32 +0100

    add test parameter

--- extrude.sh
+++ extrude.sh
@@ -1,2 +1,2 @@
-NekMesh -m extrude:layers=90 outerRegion.xml 3DoutUcomp.xml:xml:uncompress
-NekMesh -m extrude:layers=20 inFoil.xml 3DinUcomp.xml:xml:uncompress
+NekMesh -m extrude:layers=4 outerRegion.xml 3DoutUcomp.xml:xml:uncompress
+NekMesh -m extrude:layers=2 inFoil.xml 3DinUcomp.xml:xml:uncompress
