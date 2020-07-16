0001ee90fef6b0288050b88b1e5dd8ee20571f05
Ankang Gao <agao@blackfriars.ae.ic.ac.uk>
2020-07-16 22:47:49 +0100

    reduce the mesh fro Re400

--- extrude.sh
+++ extrude.sh
@@ -1,2 +1,2 @@
-NekMesh -m extrude:layers=5 outerRegion.xml 3DoutUcomp.xml:xml:uncompress
-NekMesh -m extrude:layers=2 inFoil.xml 3DinUcomp.xml:xml:uncompress
+NekMesh -m extrude:layers=25 outerRegion.xml 3DoutUcomp.xml:xml:uncompress
+NekMesh -m extrude:layers=5 inFoil.xml 3DinUcomp.xml:xml:uncompress
