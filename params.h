7e3dd4c2bbf3d7f208c29493710a2e384ef3f7eb
Ankang Gao <agao@blackfriars.ae.ic.ac.uk>
2020-07-16 16:53:13 +0100

    remove test

--- params.h
+++ params.h
@@ -18,43 +18,43 @@ double chamber = FOILM*0.01;
 double chamberp = FOILP*0.1;
 double Thickness = FOILT*0.01;
 
-double xInFoil = 0.1;
+double xInFoil = 0.04;
 double hFirstLayerInFoil = 0.01;
 double progressInFoil = 1.01;
 double rBoundaryLayerInFoil = 1.;
 double maxLayerhInFoil = 0.02;
 
-double hFirstLayer = 0.1;
-double progress = 2.;
-double rBoundaryLayer = 0.2;
-double maxLayerh = 0.7;
+double hFirstLayer = 0.015;
+double progress = 1.2;
+double rBoundaryLayer = 0.7;
+double maxLayerh = 0.075;
 
 double chordLen= 1.;
 double xmidLow1 = 0.2;
 double xmidUp1 = 0.2;
 double xmidLow2 = 0.006;
-double xmidUp2  = 0.006;
-int nLow1 = 2;
-int nLow2 = 1;
-int nUp1 = 2;
-int nUp2 = 1;
-int nFront = 1;
+double xmidUp2 = 0.006;
+int nLow1 = 23;
+int nLow2 = 8;
+int nUp1 = 24;
+int nUp2 = 8;
+int nFront = 8;
 double hTrailingEdge = hFirstLayer*0.8;
 
 double wakeLen = 1.;
-double wakeyUp = 0.5*Thickness;
-double wakeDown =-0.5*Thickness;
-int nWake = 1;
-double nearWakeDiffuseAngle = 10./180.*M_PI;
+double wakeyUp  = 0.133*Thickness;
+double wakeDown =-0.133*Thickness;
+int nWake = 20;
+double nearWakeDiffuseAngle = 0./180.*M_PI;
 
-double xBoxLeft = -2.;
-double xBoxRight = 4.;
-double yBoxUp = 3.;
-double yBoxDown = -3.;
-int nBoxLeft = 2;
-int nBoxRight = 2;
-int nBoxUp = 1;
-int nBoxDown = 1;
+double xBoxLeft = -40.;
+double xBoxRight = 40.;
+double yBoxUp = 40.;
+double yBoxDown = -40.;
+int nBoxLeft = 8;
+int nBoxRight = 8;
+int nBoxUp = 8;
+int nBoxDown = 8;
 
 int nFarWakex = 36;
 int nFarWakey = 30;
