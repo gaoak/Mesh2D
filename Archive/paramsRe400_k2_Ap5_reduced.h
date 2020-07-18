4969e54b5509cf8a5b44d888e86e5c42e88e3ffc
Ankang Gao <agao@blackfriars.ae.ic.ac.uk>
2020-07-18 02:00:19 +0100

    refined 4, archive reduced mesh

--- Archive/paramsRe400_k2_Ap5_reduced.h
+++ Archive/paramsRe400_k2_Ap5_reduced.h
@@ -18,7 +18,7 @@ double chamber = FOILM*0.01;
 double chamberp = FOILP*0.1;
 double Thickness = FOILT*0.01;
 
-double xInFoil = 0.04;
+double xInFoil = 0.02;
 double hFirstLayerInFoil = 0.01;
 double progressInFoil = 1.1;
 double rBoundaryLayerInFoil = 1.;
@@ -27,41 +27,41 @@ double infoilRatio = 0.03;
 
 double hFirstLayer = 0.015;
 double progress = 1.6;
-double rBoundaryLayer = 0.7;
-double maxLayerh = 0.15;
+double rBoundaryLayer = 1.;
+double maxLayerh = 0.12;
 
 double chordLen= 1.;
 double xmidLow1 = 0.2;
 double xmidUp1 = 0.2;
 double xmidLow2 = 0.006;
 double xmidUp2 = 0.006;
-int nLow1 = 13;
-int nLow2 = 5;
+int nLow1 = 14;
+int nLow2 = 6;
 int nUp1 = 14;
-int nUp2 = 5;
-int nFront = 5;
-double hTrailingEdge = hFirstLayer*1.;
+int nUp2 = 6;
+int nFront = 7;
+double hTrailingEdge = hFirstLayer*2.;
 
 double wakeLen = 1.;
-double wakeyUp  = 0.133*Thickness;
-double wakeDown =-0.133*Thickness;
-int nWake = 20;
+double wakeyUp  = 0.2*Thickness;
+double wakeDown =-0.2*Thickness;
+int nWake = 10;
 double nearWakeDiffuseAngle = 0./180.*M_PI;
 
 double xBoxLeft = -40.;
 double xBoxRight = 40.;
 double yBoxUp = 40.;
 double yBoxDown = -40.;
-int nBoxLeft = 4;
-int nBoxRight = 4;
-int nBoxUp = 4;
-int nBoxDown = 4;
+int nBoxLeft = 2;
+int nBoxRight = 2;
+int nBoxUp = 2;
+int nBoxDown = 2;
 
-int nFarWakex = 16;
+int nFarWakex = 15;
 int nFarWakey = 18;
 double farWakeAoA = 10./180.*M_PI;
 double wakeDiffuseAngle = 20./180.*M_PI;
-double farWakeRight = 5.;
-double farWakeUp = 1.5;
-double farWakeDown = -1.5;
+double farWakeRight = 6.;
+double farWakeUp = 1.0;
+double farWakeDown = -2.0;
 #endif
