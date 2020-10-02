6d47c90385bd1152701a1775588cc7c2621f759b
ankang1 <ankang1@v.systems>
2020-10-02 22:21:07 +0100

    refine z

--- extrude.sh
+++ extrude.sh
@@ -1,2 +1,2 @@
-NekMesh -m extrude:layers=46 outerRegion.xml 3DoutUcomp.xml:xml:uncompress
-NekMesh -m extrude:layers=8 inFoil.xml 3DinUcomp.xml:xml:uncompress
+NekMesh -m extrude:layers=50 outerRegion.xml 3DoutUcomp.xml:xml:uncompress
+NekMesh -m extrude:layers=10 inFoil.xml 3DinUcomp.xml:xml:uncompress
