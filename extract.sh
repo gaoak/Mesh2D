2a8194c6a5d2345b3461226d184a964a4570c0fc
ankang1 <ankang1@v.systems>
2020-10-18 16:55:16 +0100

    check BC

---
+++ extract.sh
@@ -0,0 +1,6 @@
+for((i=0;i<5;++i))
+do
+rm surf${i}.plt
+NekMesh -m extract:surf=${i} outerRegion.xml surf${i}.xml
+FieldConvert surf${i}.xml surf${i}.plt
+done
--- params.h
+++ params.h
@@ -1,5 +1,5 @@
-//for 3D simulation of Re400, k2, A=0.5, mesh is reduced to extreme,
-//flexible boundary thickness, with 1500 elements
+//for 2D simulation of Re <= 1000, k2, A=0.5, mesh is reduced to extreme,
+//flexible boundary thickness, with 8500 elements
 #ifndef PARAMS_H
 #define PARAMS_H
 #define NUMPTS 20
