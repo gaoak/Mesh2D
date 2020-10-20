1d23c0b37197ba76be9d745032a60102a194b299
ankang1 <ankang1@AnkangGao.local>
2020-10-20 16:21:23 +0100

    complete

--- make.sh
+++ make.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 for aoa in  15
 do
-    c++ -std=c++11 -DOUTPUTEXP -DFOILAOA=${aoa} -DFOILM=0 -DFOILP=0 -DFOILT=12  *.cpp CAD2D/*.cpp -c -Og -g
+    c++ -std=c++11 *.cpp CAD2D/*.cpp -c -Og -g
     c++ -std=c++11 *.o -Og -o mesh-g -g
-    ./mesh-g wake
-    echo "gmsh FarField.geo"
-    echo "Mesh.RecombinationAlgorithm = 1;" >> FarField.geo
-    ${GMSHBIN}gmsh FarField.geo  -2 -algo del2d  -clscale 10 -smooth 5  > logmesh.dat
-    echo "Mesh.RecombinationAlgorithm = 0;" >> airfoil.geo
-    echo "gmsh airfoil.geo"
-    ${GMSHBIN}gmsh airfoil.geo  -2 -algo meshadapt -clscale 10  >> logmesh.dat
-    ./mesh-g wake merge FarField.msh airfoil.msh
-    rm *plt *vtu
-    $NEKBIN/FieldConvert outerRegion.xml outerRegion.plt
-    $NEKBIN/FieldConvert outerRegion_Otip.xml outerRegion_Otip.plt
-    $NEKBIN/FieldConvert inFoil.xml inFoil.plt
+    ./mesh-g
 done
