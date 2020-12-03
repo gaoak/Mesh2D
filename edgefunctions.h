0db10b6b38070f8c4de3449e6eec1aeb14db9ec2
you <you@example.com>
2020-12-03 13:09:41 +0000

    tmp

--- edgefunctions.h
+++ edgefunctions.h
@@ -3,37 +3,102 @@
 #include "params.h"
 
 double pts[NUMPTS][2];
+int findNlayers(double h, double q, double R, double m){
+    int n = 0;
+    double len = 0;
+    double delta = h;
+    for(n=1;n<=1000000; ++n) {
+        if(delta>=m) delta = m;
+        len += delta;
+        if(len>=R) return n;
+        delta *= q;
+    }
+    return n;
+}
+
+int N1x = std::ceil((p2x-p1x)/meshsize);
+int N1y = findNlayers(hfirstlayer, hgrowth, p4y-p1y, meshsize);
 
 int InitPts(){
-    pts[0][0] = xleft;
-    pts[0][1] = ylow;
+    pts[0][0] = p0x;
+    pts[0][1] = p0y;
     
-    pts[1][0] = xright;
-    pts[1][1] = ylow;
+    pts[1][0] = p1x;
+    pts[1][1] = p1y;
     
-    pts[2][0] = xright;
-    pts[2][1] = yup;
+    pts[2][0] = p2x;
+    pts[2][1] = p2y;
     
-    pts[3][0] = xleft;
-    pts[3][1] = yup;
+    pts[3][0] = p3x;
+    pts[3][1] = p3y;
+
+    pts[4][0] = p4x;
+    pts[4][1] = p4y;
+
+    pts[5][0] = p5x;
+    pts[5][1] = p5y;
+
+    pts[6][0] = p6x;
+    pts[6][1] = p6y;
+
+    pts[7][0] = p7x;
+    pts[7][1] = p7y;
+
+    pts[8][0] = p8x;
+    pts[8][1] = p8y;
     return 0;
 }
 
-LineEdge Cedge0(pts[0], pts[1], Nx, UNIFORM, 0., 0.);
-LineEdge Cedge1(pts[1], pts[2], Ny, UNIFORM, 0., 0.);
-LineEdge Cedge2(pts[3], pts[2], Nx, UNIFORM, 0., 0.);
-LineEdge Cedge3(pts[0], pts[3], Ny, UNIFORM, 0., 0.);
+LineEdge Cedge12(pts[1], pts[2], N1x, UNIFORM, 0., 0.);
+LineEdge Cedge45(pts[4], pts[5], N1x, UNIFORM, 0., 0.);
+LineEdge Cedge78(pts[7], pts[8], N1x, UNIFORM, 0., 0.);
+std::vector<double> edge12(double s) {
+    return Cedge12.Evaluate(s);
+}
+std::vector<double> edge45(double s) {
+    return Cedge45.Evaluate(s);
+}
+std::vector<double> edge78(double s) {
+    return Cedge78.Evaluate(s);
+}
 
-std::vector<double> edge0(double s) {
-    return Cedge0.Evaluate(s);
+LineEdge Cedge25(pts[2], pts[5], N1y, BOUNDARYLAYER0, hfirstlayer, hgrowth, N1y-1, 0, 0, 0);
+LineEdge Cedge14(pts[1], pts[4], N1y, BOUNDARYLAYER0, hfirstlayer, hgrowth, N1y-1, 0, 0, 0);
+LineEdge Cedge03(pts[0], pts[3], N1y, BOUNDARYLAYER0, hfirstlayer, hgrowth, N1y-1, 0, 0, 0);
+std::vector<double> edge25(double s) {
+    return Cedge25.Evaluate(s);
 }
-std::vector<double> edge1(double s) {
-    return Cedge1.Evaluate(s);
+std::vector<double> edge14(double s) {
+    return Cedge14.Evaluate(s);
 }
-std::vector<double> edge2(double s) {
-    return Cedge2.Evaluate(s);
+std::vector<double> edge03(double s) {
+    return Cedge03.Evaluate(s);
 }
-std::vector<double> edge3(double s) {
-    return Cedge3.Evaluate(s);
+
+LineEdge Cedge58(pts[5], pts[8], N2y, BOUNDARYLAYER0, meshsize, hgrowth, N2y-1, 0, 0, 0);
+LineEdge Cedge47(pts[4], pts[7], N2y, BOUNDARYLAYER0, meshsize, hgrowth, N2y-1, 0, 0, 0);
+LineEdge Cedge36(pts[3], pts[6], N2y, BOUNDARYLAYER0, meshsize, hgrowth, N2y-1, 0, 0, 0);
+std::vector<double> edge58(double s) {
+    return Cedge58.Evaluate(s);
 }
+std::vector<double> edge47(double s) {
+    return Cedge47.Evaluate(s);
+}
+std::vector<double> edge36(double s) {
+    return Cedge36.Evaluate(s);
+}
+
+LineEdge Cedge01(pts[0], pts[1], N1y, BOUNDARYLAYER1, 0, 0, 0, (p2x-p1x)/N1x, hgrowth, 10);
+LineEdge Cedge34(pts[3], pts[4], N1y, BOUNDARYLAYER1, 0, 0, 0, (p5x-p4x)/N1x, hgrowth, 10);
+LineEdge Cedge67(pts[6], pts[7], N1y, BOUNDARYLAYER1, 0, 0, 0, (p8x-p7x)/N1x, hgrowth, 10);
+std::vector<double> edge01(double s) {
+    return Cedge01.Evaluate(s);
+}
+std::vector<double> edge34(double s) {
+    return Cedge34.Evaluate(s);
+}
+std::vector<double> edge67(double s) {
+    return Cedge67.Evaluate(s);
+}
+
 #endif
--- mesh.cpp
+++ mesh.cpp
@@ -14,24 +14,54 @@ int main(int argc, char* argv[])
 {
     InitPts();
     std::vector<void*> edges0;
-    edges0.push_back((void*)edge0);
-    edges0.push_back((void*)edge1);
-    edges0.push_back((void*)edge2);
-    edges0.push_back((void*)edge3);
-    RectRegion reg(edges0, "square");
-    reg.MeshGen(Cedge0.m_N, Cedge1.m_N);
-    reg.Tec360Pts("test2.dat");
+    edges0.push_back((void*)edge12);
+    edges0.push_back((void*)edge25);
+    edges0.push_back((void*)edge45);
+    edges0.push_back((void*)edge14);
+    RectRegion reg1(edges0, "reg1");
+    reg1.MeshGen(Cedge12.m_N, Cedge25.m_N);
+
+    edges0.clear();
+    edges0.push_back((void*)edge01);
+    edges0.push_back((void*)edge14);
+    edges0.push_back((void*)edge34);
+    edges0.push_back((void*)edge03);
+    RectRegion reg0(edges0, "reg0");
+    reg0.MeshGen(Cedge01.m_N, Cedge14.m_N);
+
+    edges0.clear();
+    edges0.push_back((void*)edge34);
+    edges0.push_back((void*)edge47);
+    edges0.push_back((void*)edge67);
+    edges0.push_back((void*)edge36);
+    RectRegion reg2(edges0, "reg2");
+    reg2.MeshGen(Cedge34.m_N, Cedge47.m_N);
+
+    edges0.clear();
+    edges0.push_back((void*)edge45);
+    edges0.push_back((void*)edge58);
+    edges0.push_back((void*)edge78);
+    edges0.push_back((void*)edge47);
+    RectRegion reg3(edges0, "reg3");
+    reg3.MeshGen(Cedge45.m_N, Cedge58.m_N);
 
     MeshRegions combinedReg("domain", 1E-6);
-    combinedReg.AddRegion(reg);
+    combinedReg.AddRegion(reg1);
+    combinedReg.AddRegion(reg0);
+    combinedReg.AddRegion(reg2);
+    combinedReg.AddRegion(reg3);
 
     vector<int> comp2;
     comp2.push_back(0);
     //inlet
-    combinedReg.defineBoundary((void*)edge0,  Cedge0.m_N, 1);
-    combinedReg.defineBoundary((void*)edge1 , Cedge1.m_N, 2);
-    combinedReg.defineBoundary((void*)edge2,  Cedge2.m_N, 3);
-    combinedReg.defineBoundary((void*)edge3,  Cedge3.m_N, 4);
+    combinedReg.defineBoundary((void*)edge01,  Cedge01.m_N, 1);
+    combinedReg.defineBoundary((void*)edge12,  Cedge12.m_N, 1);
+    combinedReg.defineBoundary((void*)edge67 , Cedge67.m_N, 2);
+    combinedReg.defineBoundary((void*)edge78 , Cedge78.m_N, 2);
+    combinedReg.defineBoundary((void*)edge03,  Cedge03.m_N, 3);
+    combinedReg.defineBoundary((void*)edge36,  Cedge36.m_N, 3);
+    combinedReg.defineBoundary((void*)edge25,  Cedge25.m_N, 4);
+    combinedReg.defineBoundary((void*)edge58,  Cedge58.m_N, 4);
     //output
     combinedReg.outXml("square.xml");
     combinedReg.outCOMPO("square.xml", comp2);
