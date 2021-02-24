6cdfbd804e4430eb987e833b30101ee6c75a4081
ankang <ankang@ubuntu.com>
2021-02-24 19:05:50 +0000

    half

--- Archive/isolatevortex.h
+++ Archive/isolatevortex.h
@@ -7,8 +7,8 @@ double hgrowth = 1.5;
 double meshsize = 0.025;
 
 double p0x = 0.;
-double p1x = 2.;
-double p2x = 5.;
+double p1x = meshsize;
+double p2x = 3.;
 double p3x = 7.;
 
 double p0y = 0.;
@@ -18,7 +18,7 @@ double p3y = 5.;
 
 int N0y = 6;
 int N2y = 6;
-int N0x = 6;
+int N0x = 1;
 int N2x = 6;
 
 #endif
