a87e7d96f30a5938da26514f6c152902668d1fe3
Ankang Gao <ankang.gao@ustc.edu.cn>
2022-09-30 16:11:08 +0800

    add addedmass for flapping

--- params.h
+++ params.h
@@ -1,9 +1,9 @@
-//for 2D simulation of Re <= 1000, k2, A=0.5, mesh is reduced to extreme,
-//flexible boundary thickness, with 8500 elements
+//for 2D simulation of Re <= 1000, k2, A=0.5, mesh is refined,
+//flexible boundary thickness, with 6377 elements
 #ifndef PARAMS_H
 #define PARAMS_H
 #define NUMPTS 20
-static double FOILAOA = 15.;// AoA
+static double FOILAOA = 0.;// AoA
 static double FOILM   = 0.; // NACA 0
 static double FOILP   = 0.; // 0
 static double FOILT   = 12.;// 12
@@ -17,20 +17,20 @@ double xInFoil = 0.01;
 double hFirstLayerInFoil = 0.01;
 double progressInFoil = 1.6;
 double rBoundaryLayerInFoil = 1.;
-double maxLayerhInFoil = 0.04;
+double maxLayerhInFoil = 0.07;
 double infoilRatio = 0.03;
 
 double hFirstLayer = 0.01;
-double progress = 1.4;
-double rBoundaryLayer  = 0.5;
-double rBoundaryLayer1 = 0.5;
-double rBoundaryLayer2 = 0.5;
-double rBoundaryLayer3 = 1.;
-double rBoundaryLayer4 = 1.;
-double rBoundaryLayer5 = 1.;
-double rBoundaryLayer6 = 1.;
-double rBoundaryLayer7 = 1.;
-double maxLayerh = 0.05;
+double progress = 1.3;
+double rBoundaryLayer  = 0.8;
+double rBoundaryLayer1 = 0.8;
+double rBoundaryLayer2 = 0.8;
+double rBoundaryLayer3 = 0.8;
+double rBoundaryLayer4 = 0.8;
+double rBoundaryLayer5 = 0.8;
+double rBoundaryLayer6 = 0.8;
+double rBoundaryLayer7 = 0.8;
+double maxLayerh = 0.07;
 
 double chordLen= 1.;
 double xmidLow1 = 0.2;
@@ -39,9 +39,9 @@ double xmidLow2 = 0.006;
 double growthrateLow2 = 1.3;
 double xmidUp2 = 0.006;
 double growthrateUp2 = 1.3;
-int nLow1 = 28;
+int nLow1 = 20;
 int nLow2 = 13;
-int nUp1 = 28;
+int nUp1 = 20;
 int nUp2 = 13;
 int nFront = 12;
 
@@ -60,10 +60,10 @@ int nBoxRight = 10;
 int nBoxUp = 10;
 int nBoxDown = 10;
 
-int nFarWakex = 72;
-int nFarWakey = 36;
-double farWakeAoA = 10./180.*M_PI;
-double wakeDiffuseAngle = 14./180.*M_PI;
+int nFarWakex = 80;
+int nFarWakey = 40;
+double farWakeAoA = 0./180.*M_PI;
+double wakeDiffuseAngle = 10./180.*M_PI;
 double farWakeRight = 20.;
 double farWakeUp = 1.5;
 double farWakeDown = -1.5;
