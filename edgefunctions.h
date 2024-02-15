8c198b733f6d1e4f3a28fe41c764cd40e0c217d8
Ankang Gao <ankanggao@ustc.edu.cn>
2024-02-15 22:47:03 +0800

    save work

--- edgefunctions.h
+++ edgefunctions.h
@@ -2,181 +2,68 @@
 #define EDGEFUNCTIONS_H
 #include "params.h"
 
-double g_ptsA[20][2];
-double g_ptsW[20][2];
-double g_ptsF[20][2];
-double g_thetaA[20][2];
-int nLayersU0 = findNlayers(hFirstLayer, progress, upperBL0, maxLayerh);
-int nLayersU1 = findNlayers(hFirstLayer, progress, upperBL1, maxLayerh);
-int nLayersU2 = findNlayers(hFirstLayer, progress, upperBL2, maxLayerh);
-int nLayersU3 = findNlayers(hFirstLayer, progress, upperBL3, maxLayerh);
-int nLayersU4 = findNlayers(hFirstLayer, progress, upperBL4, maxLayerh);
-int nLayersU5 = findNlayers(hFirstLayer, progress, upperBL5, maxLayerh);
-
-int nLayersL0 = findNlayers(hFirstLayer, progress, lowerBL0, maxLayerh);
-int nLayersL1 = findNlayers(hFirstLayer, progress, lowerBL1, maxLayerh);
-int nLayersL2 = findNlayers(hFirstLayer, progress, lowerBL2, maxLayerh);
-int nLayersL3 = findNlayers(hFirstLayer, progress, lowerBL3, maxLayerh);
-int nLayersL4 = findNlayers(hFirstLayer, progress, lowerBL4, maxLayerh);
-int nLayersL5 = findNlayers(hFirstLayer, progress, lowerBL5, maxLayerh);
+double g_ptsC[20][2];
+int nBLayers = findNlayers(hFirstLayer, progress, wallBLThickness0, maxBLSize);
 
 int InitPts() {
-  setRadiusMesh(hFirstLayer, progress, maxLayerh);
   // airfoil
-  g_thetaA[0][0] = 0.5 * M_PI;
-  g_thetaA[4][0] = 0.5 * M_PI;
-  g_thetaA[5][0] = 0.;
-  g_thetaA[6][0] = -0.5 * M_PI;
-  g_thetaA[10][0] = 1.5 * M_PI;
-  g_thetaA[11][0] = M_PI;
+  g_ptsC[0][0] = radiusL*cos(theta0);
+  g_ptsC[0][1] = radiusL*sin(theta0);
+  g_ptsC[1][0] = radiusL*cos(theta1);
+  g_ptsC[1][1] = radiusL*sin(theta1);
+  g_ptsC[2][0] = radiusL*cos(theta2);
+  g_ptsC[2][1] = radiusL*sin(theta2);
 
-  g_ptsA[0][0] = 0.5 * Thickness;
-  g_ptsA[1][0] = upperx1;
-  g_ptsA[2][0] = upperx2;
-  g_ptsA[3][0] = upperx3;
-  g_ptsA[4][0] = 1. - 0.5 * Thickness;
-  g_ptsA[0][1] = 0.5 * Thickness;
-  g_ptsA[1][1] = 0.5 * Thickness;
-  g_ptsA[2][1] = 0.5 * Thickness;
-  g_ptsA[3][1] = 0.5 * Thickness;
-  g_ptsA[4][1] = 0.5 * Thickness;
-
-  g_ptsA[6][0] = 1. - 0.5 * Thickness;
-  g_ptsA[7][0] = lowerx3;
-  g_ptsA[8][0] = lowerx2;
-  g_ptsA[9][0] = lowerx1;
-  g_ptsA[10][0] = 0.5 * Thickness;
-  g_ptsA[6][1] = -0.5 * Thickness;
-  g_ptsA[7][1] = -0.5 * Thickness;
-  g_ptsA[8][1] = -0.5 * Thickness;
-  g_ptsA[9][1] = -0.5 * Thickness;
-  g_ptsA[10][1] = -0.5 * Thickness;
-  // far boundary
-  double deltax = (xBoxRight - xBoxLeft) / nBoxDown;
-  double deltay = (yBoxUp - yBoxDown) / nBoxLeft;
-  g_ptsF[0][0] = xBoxLeft;
-  g_ptsF[1][0] = xBoxLeft + deltax;
-  g_ptsF[2][0] = xBoxRight - deltax;
-  g_ptsF[3][0] = xBoxRight;
-  g_ptsF[0][1] = yBoxDown;
-  g_ptsF[1][1] = yBoxDown;
-  g_ptsF[2][1] = yBoxDown;
-  g_ptsF[3][1] = yBoxDown;
-
-  g_ptsF[8][0] = xBoxLeft;
-  g_ptsF[9][0] = xBoxLeft + deltax;
-  g_ptsF[10][0] = xBoxRight - deltax;
-  g_ptsF[11][0] = xBoxRight;
-  g_ptsF[8][1] = yBoxUp;
-  g_ptsF[9][1] = yBoxUp;
-  g_ptsF[10][1] = yBoxUp;
-  g_ptsF[11][1] = yBoxUp;
-
-  g_ptsF[4][0] = xBoxLeft + deltax;
-  g_ptsF[5][0] = xBoxRight - deltax;
-  g_ptsF[4][1] = yBoxDown + deltay;
-  g_ptsF[5][1] = yBoxDown + deltay;
-
-  g_ptsF[6][0] = xBoxLeft + deltax;
-  g_ptsF[7][0] = xBoxRight - deltax;
-  g_ptsF[6][1] = yBoxUp - deltay;
-  g_ptsF[7][1] = yBoxUp - deltay;
-  // wake
-  g_ptsW[0][0] = farWakeCx + 0.5 * farWakeHeight * sin(farWakeAoA);
-  g_ptsW[0][1] = farWakeCy - 0.5 * farWakeHeight * cos(farWakeAoA);
-  g_ptsW[3][0] = farWakeCx - 0.5 * farWakeHeight * sin(farWakeAoA);
-  g_ptsW[3][1] = farWakeCy + 0.5 * farWakeHeight * cos(farWakeAoA);
-  double cx = farWakeCx + farWakeLength * cos(farWakeAoA);
-  double cy = farWakeCy + farWakeLength * sin(farWakeAoA);
-  double height =
-      farWakeHeight + 2. * farWakeLength * tan(wakeDiffuseAngle * 0.5);
-  g_ptsW[1][0] = cx + 0.5 * height * sin(farWakeAoA);
-  g_ptsW[1][1] = cy - 0.5 * height * cos(farWakeAoA);
-  g_ptsW[2][0] = cx - 0.5 * height * sin(farWakeAoA);
-  g_ptsW[2][1] = cy + 0.5 * height * cos(farWakeAoA);
+  g_ptsC[3][0] = 0.;
+  g_ptsC[3][1] = 0.;
+  g_ptsC[4][0] = 0.;
+  g_ptsC[4][1] = 0.;
   return 0;
 }
 
-LineEdge Cedge1011(g_thetaA[10], g_thetaA[11], nLow0, UNIFORM, 0., 0.);
-LineEdge Cedge110(g_thetaA[11], g_thetaA[0], nUp0, UNIFORM, 0., 0.);
-LineEdge Cedge45(g_thetaA[4], g_thetaA[5], nUp5, UNIFORM, 0., 0.);
-LineEdge Cedge56(g_thetaA[5], g_thetaA[6], nLow5, UNIFORM, 0., 0.);
-std::vector<double> edge1011(double s) {
-  double x0 = 0.5 * Thickness, radius = 0.5 * Thickness;
-  double t = Cedge1011.Evaluate(s)[0];
+std::vector<double> edge0(double s) {
+  double t = -M_PI*s;
+  double radius = ChordLen * 0.5;
   std::vector<double> res(2, 0.);
-  res[0] = x0 + radius * cos(t);
-  res[1] = radius * sin(t);
+  res[0] = g_ptsC[0][0] + radius * cos(t);
+  res[1] = g_ptsC[0][1] + radius * sin(t);
   return res;
 }
-std::vector<double> edge110(double s) {
-  double x0 = 0.5 * Thickness, radius = 0.5 * Thickness;
-  double t = Cedge110.Evaluate(s)[0];
+std::vector<double> edge1(double s) {
+  double t = -M_PI*s;
+  double radius = ChordLen * 0.5;
   std::vector<double> res(2, 0.);
-  res[0] = x0 + radius * cos(t);
-  res[1] = radius * sin(t);
+  res[0] = g_ptsC[1][0] + radius * cos(t);
+  res[1] = g_ptsC[1][1] + radius * sin(t);
   return res;
 }
-std::vector<double> edge45(double s) {
-  double x0 = 1. - 0.5 * Thickness, radius = 0.5 * Thickness;
-  double t = Cedge45.Evaluate(s)[0];
+std::vector<double> edge2(double s) {
+  double t = -M_PI*s;
+  double radius = ChordLen * 0.5;
   std::vector<double> res(2, 0.);
-  res[0] = x0 + radius * cos(t);
-  res[1] = radius * sin(t);
-  return res;
-}
-std::vector<double> edge56(double s) {
-  double x0 = 1. - 0.5 * Thickness, radius = 0.5 * Thickness;
-  double t = Cedge56.Evaluate(s)[0];
-  std::vector<double> res(2, 0.);
-  res[0] = x0 + radius * cos(t);
-  res[1] = radius * sin(t);
+  res[0] = g_ptsC[2][0] + radius * cos(t);
+  res[1] = g_ptsC[2][1] + radius * sin(t);
   return res;
 }
 
-LineEdge Cedge01(g_ptsA[0], g_ptsA[1], nUp1, BOUNDARYLAYER0, hFirstLayer, 1.6,
-                 5, 0., 0., 0);
-LineEdge Cedge12(g_ptsA[1], g_ptsA[2], nUp2, UNIFORM, 0., 0.);
-LineEdge Cedge23(g_ptsA[2], g_ptsA[3], nUp3, UNIFORM, 0., 0.);
-LineEdge Cedge34(g_ptsA[3], g_ptsA[4], nUp4, BOUNDARYLAYER1, 0., 0., 0,
-                 hFirstLayer, 1.6, 5);
-std::vector<double> edge01(double s) { return Cedge01.Evaluate(s); }
-std::vector<double> edge12(double s) { return Cedge12.Evaluate(s); }
-std::vector<double> edge23(double s) { return Cedge23.Evaluate(s); }
-std::vector<double> edge34(double s) { return Cedge34.Evaluate(s); }
+std::vector<double> edge3(double s) {
+  double t = M_PI*s;
+  std::vector<double> res(2, 0.);
+  res[0] = g_ptsC[3][0] + radiusNear * cos(t);
+  res[1] = g_ptsC[3][1] + radiusNear * sin(t);
+  return res;
+}
+std::vector<double> edge4(double s) {
+  double t = M_PI*s;
+  std::vector<double> res(2, 0.);
+  res[0] = g_ptsC[4][0] + radiusFar * cos(t);
+  res[1] = g_ptsC[4][1] + radiusFar * sin(t);
+  return res;
+}
 
-LineEdge Cedge67(g_ptsA[6], g_ptsA[7], nLow4, BOUNDARYLAYER0, hFirstLayer, 1.6,
-                 5, 0., 0., 0);
-LineEdge Cedge78(g_ptsA[7], g_ptsA[8], nLow3, UNIFORM, 0., 0.);
-LineEdge Cedge89(g_ptsA[8], g_ptsA[9], nLow2, UNIFORM, 0., 0.);
-LineEdge Cedge910(g_ptsA[9], g_ptsA[10], nLow1, BOUNDARYLAYER1, 0., 0., 0,
-                  hFirstLayer, 1.6, 5);
-std::vector<double> edge67(double s) { return Cedge67.Evaluate(s); }
-std::vector<double> edge78(double s) { return Cedge78.Evaluate(s); }
-std::vector<double> edge89(double s) { return Cedge89.Evaluate(s); }
-std::vector<double> edge910(double s) { return Cedge910.Evaluate(s); }
-
-LineEdge Cwake01(g_ptsW[0], g_ptsW[1], nFarWakex, QUDREFINE0,
-                 farWakeHeight / nFarWakey, 0.);
-LineEdge Cwake12(g_ptsW[1], g_ptsW[2], nFarWakey, UNIFORM, 0., 0.);
-LineEdge Cwake23(g_ptsW[2], g_ptsW[3], nFarWakex, QUDREFINE1, 0.,
-                 farWakeHeight / nFarWakey);
-LineEdge Cwake30(g_ptsW[3], g_ptsW[0], nFarWakey, UNIFORM, 0., 0.);
-std::vector<double> wake01(double s) { return Cwake01.Evaluate(s); }
-std::vector<double> wake12(double s) { return Cwake12.Evaluate(s); }
-std::vector<double> wake23(double s) { return Cwake23.Evaluate(s); }
-std::vector<double> wake30(double s) { return Cwake30.Evaluate(s); }
-
-LineEdge Cfar03(g_ptsF[0], g_ptsF[3], nBoxDown, UNIFORM, 0., 0.);
-LineEdge Cfar811(g_ptsF[8], g_ptsF[11], nBoxUp, UNIFORM, 0., 0.);
-LineEdge Cfar08(g_ptsF[0], g_ptsF[8], nBoxLeft, UNIFORM, 0., 0.);
-LineEdge Cfar311(g_ptsF[3], g_ptsF[11], nBoxRight, UNIFORM, 0., 0.);
-std::vector<double> far03(double s) { return Cfar03.Evaluate(s); }
-std::vector<double> far811(double s) { return Cfar811.Evaluate(s); }
-std::vector<double> far08(double s) { return Cfar08.Evaluate(s); }
-std::vector<double> far311(double s) { return Cfar311.Evaluate(s); }
-
-std::vector<double> g_boundingbox;
+std::vector<double> g_boundingbox0;
+std::vector<double> g_boundingbox1;
+std::vector<double> g_boundingbox2;
 bool toremove(std::vector<double> p) {
   double eps = g_boundingbox[g_boundingbox.size() - 1];
   for (int i = 0; i < 2; ++i) {
@@ -187,4 +74,5 @@ bool toremove(std::vector<double> p) {
   }
   return true;
 }
+
 #endif
--- mesh.cpp
+++ mesh.cpp
@@ -14,7 +14,6 @@ using namespace std;
 #include "edgefunctions.h"
 int meshingNearBody(MeshRegions &combinedReg);
 int meshingBoundaryLayer(MeshRegions &combinedReg);
-int meshingWake(MeshRegions &combinedReg);
 int outputXML(MeshRegions &combinedReg);
 int outputGeo(MeshRegions &combinedReg, std::vector<int> OutLevels);
 int meshingOuterBoundary(MeshRegions &combinedReg);
@@ -36,7 +35,6 @@ int main(int argc, char *argv[]) {
   MeshRegions combinedReg("RComb_", 1.E-6);
   meshingBoundaryLayer(combinedReg);
   meshingNearBody(combinedReg);
-  meshingWake(combinedReg);
   meshingOuterBoundary(combinedReg);
   if (!merge) {
     std::vector<int> OutLevels = {1, 3};
@@ -77,89 +75,51 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+int meshingNearBody(MeshRegions &combinedReg) {
+  setRadiusMesh(2.*M_PI*radiusNeari/Nnear, 1.01, 2.*M_PI*radiusNeari/Nnear);
+  std::vector<void *> edges0;
+  void *edgetmp;
+  // edge 11-0
+  edges0.push_back((void *)edge3);
+  edges0.push_back((void *)radiusEdge);
+  edges0.push_back(edgetmp);
+  edges0.push_back(edgetmp);
+  RectRegion Rect(RectRegion(edges0, "cyl3", false));
+  setRadiusLayers(1);
+  Rect.MeshGen(Nnear, 1, eBoundaryLayer1);
+  Rect.Tec360Pts("cyl3.dat");
+  combinedReg.AddRegion(Rect);
+  return 0;
+}
+
 int meshingBoundaryLayer(MeshRegions &combinedReg) {
+  setRadiusMesh(hFirstLayer, progress, maxBLSize);
   /////////near body region////////////////
   std::vector<RectRegion> Rects;
   // boundary layer region 0
   std::vector<void *> edges0;
-  void *edge0;
+  void *edgetmp;
   // edge 11-0
-  edges0.push_back((void *)edge110);
+  edges0.push_back((void *)edge0);
   edges0.push_back((void *)radiusEdge);
-  edges0.push_back(edge0);
-  edges0.push_back(edge0);
-  Rects.push_back(RectRegion(edges0, "up0", false));
-  setRadiusLayers(nLayersU0);
-  Rects[Rects.size() - 1].MeshGen(Cedge110.m_N, nLayersU0, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("up0.dat");
+  edges0.push_back(edgetmp);
+  edges0.push_back(edgetmp);
+  Rects.push_back(RectRegion(edges0, "cyl0", false));
+  setRadiusLayers(nBLayers);
+  Rects[Rects.size() - 1].MeshGen(Ncylinder, nBLayers, eBoundaryLayer1);
+  Rects[Rects.size() - 1].Tec360Pts("cyl0.dat");
   // edge 01
-  edges0[0] = (void *)edge01;
-  Rects.push_back(RectRegion(edges0, "up1", false));
-  setRadiusLayers(nLayersU1);
-  Rects[Rects.size() - 1].MeshGen(Cedge01.m_N, nLayersU1, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("up1.dat");
+  edges0[0] = (void *)edge1;
+  Rects.push_back(RectRegion(edges0, "cyl1", false));
+  setRadiusLayers(nBLayers);
+  Rects[Rects.size() - 1].MeshGen(Ncylinder, nBLayers, eBoundaryLayer1);
+  Rects[Rects.size() - 1].Tec360Pts("cyl1.dat");
   // edge 12
-  edges0[0] = (void *)edge12;
-  Rects.push_back(RectRegion(edges0, "up2", false));
-  setRadiusLayers(nLayersU2);
-  Rects[Rects.size() - 1].MeshGen(Cedge12.m_N, nLayersU2, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("up2.dat");
-  // edge 23
-  edges0[0] = (void *)edge23;
-  Rects.push_back(RectRegion(edges0, "up3", false));
-  setRadiusLayers(nLayersU3);
-  Rects[Rects.size() - 1].MeshGen(Cedge23.m_N, nLayersU3, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("up3.dat");
-  // edge 34
-  edges0[0] = (void *)edge34;
-  Rects.push_back(RectRegion(edges0, "up4", false));
-  setRadiusLayers(nLayersU4);
-  Rects[Rects.size() - 1].MeshGen(Cedge34.m_N, nLayersU4, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("up4.dat");
-  // edge 45
-  edges0[0] = (void *)edge45;
-  Rects.push_back(RectRegion(edges0, "up5", false));
-  setRadiusLayers(nLayersU5);
-  Rects[Rects.size() - 1].MeshGen(Cedge45.m_N, nLayersU5, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("up5.dat");
-
-  // edge 5-6
-  edges0[0] = (void *)edge56;
-  Rects.push_back(RectRegion(edges0, "low5", false));
-  setRadiusLayers(nLayersL5);
-  Rects[Rects.size() - 1].MeshGen(Cedge56.m_N, nLayersL5, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("low5.dat");
-  // edge 6-7
-  edges0[0] = (void *)edge67;
-  Rects.push_back(RectRegion(edges0, "low4", false));
-  setRadiusLayers(nLayersL4);
-  Rects[Rects.size() - 1].MeshGen(Cedge67.m_N, nLayersL4, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("low4.dat");
-  // edge 7-8
-  edges0[0] = (void *)edge78;
-  Rects.push_back(RectRegion(edges0, "low3", false));
-  setRadiusLayers(nLayersL3);
-  Rects[Rects.size() - 1].MeshGen(Cedge78.m_N, nLayersL3, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("low3.dat");
-  // edge 8-9
-  edges0[0] = (void *)edge89;
-  Rects.push_back(RectRegion(edges0, "low2", false));
-  setRadiusLayers(nLayersL2);
-  Rects[Rects.size() - 1].MeshGen(Cedge89.m_N, nLayersL2, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("low2.dat");
-  // edge 9-10
-  edges0[0] = (void *)edge910;
-  Rects.push_back(RectRegion(edges0, "low1", false));
-  setRadiusLayers(nLayersL1);
-  Rects[Rects.size() - 1].MeshGen(Cedge910.m_N, nLayersL1, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("low1.dat");
-  // edge 10-11
-  edges0[0] = (void *)edge1011;
-  Rects.push_back(RectRegion(edges0, "low0", false));
-  setRadiusLayers(nLayersL0);
-  Rects[Rects.size() - 1].MeshGen(Cedge1011.m_N, nLayersL0, eBoundaryLayer1);
-  Rects[Rects.size() - 1].Tec360Pts("low0.dat");
-
+  edges0[0] = (void *)edge2;
+  Rects.push_back(RectRegion(edges0, "cyl2", false));
+  setRadiusLayers(nBLayers);
+  Rects[Rects.size() - 1].MeshGen(Ncylinder, nBLayers, eBoundaryLayer1);
+  Rects[Rects.size() - 1].Tec360Pts("cyl2.dat");
   ///////////// combine the near field mesh
   for (unsigned int i = 0; i < Rects.size(); ++i) {
     combinedReg.AddRegion(Rects[i]);
@@ -167,113 +127,20 @@ int meshingBoundaryLayer(MeshRegions &combinedReg) {
   return 0;
 }
 
-int meshingNearBody(MeshRegions &combinedReg) {
-  MeshRegions nearBodyRegion("Rnear_", 1e-8);
-  vector<vector<double>> p = {{nearBoxLeft, nearBoxDown},
-                              {nearBoxRight, nearBoxDown},
-                              {nearBoxRight, nearBoxUp},
-                              {nearBoxLeft, nearBoxUp}};
-  vector<vector<vector<double>>> edges;
-  // left
-  vector<int> pi = {0, 1, 2, 3, 0};
-  edges.clear();
-  for (int i = 0; i < 4; ++i) {
-    vector<vector<double>> edge;
-    edge.push_back(p[pi[i]]);
-    edge.push_back(p[pi[i + 1]]);
-    edges.push_back(edge);
-  }
-  RectRegion pic0 = RectRegion(edges, "pic");
-  pic0.MeshGen(int((nearBoxRight - nearBoxLeft) / maxLayerh + 0.5),
-               int((nearBoxUp - nearBoxDown) / maxLayerh + 0.5));
-  nearBodyRegion.AddRegion(pic0);
-  nearBodyRegion.transformation(nearAoA);
-
-  combinedReg.GetBoundBox(g_boundingbox);
-  g_boundingbox.push_back(maxLayerh * 2.);
-  nearBodyRegion.RemoveElements((void *)toremove);
-  //////////////combine region//////////
-  combinedReg.AddRegion(nearBodyRegion);
-  return 0;
-}
-
-int meshingWake(MeshRegions &combinedReg) {
-  std::vector<void *> edges4;
-  edges4.push_back((void *)wake01);
-  edges4.push_back((void *)wake12);
-  edges4.push_back((void *)wake23);
-  edges4.push_back((void *)wake30);
-  RectRegion farwakeRegion = RectRegion(edges4, "R_FarWake");
-  farwakeRegion.MeshGen(Cwake01.m_N, Cwake12.m_N);
-  farwakeRegion.Tec360Pts("farwake.dat");
-  //////////////combine region//////////
-  combinedReg.AddRegion(farwakeRegion);
-  return 0;
-}
-
 int meshingOuterBoundary(MeshRegions &combinedReg) {
-  vector<int> pts;
-  vector<vector<vector<double>>> edges;
-  // left
-  pts = {0, 1, 9, 8, 0};
-  edges.clear();
-  for (int i = 0; i < 4; ++i) {
-    vector<double> p0 = {g_ptsF[pts[i]][0], g_ptsF[pts[i]][1]};
-    vector<double> p1 = {g_ptsF[pts[i + 1]][0], g_ptsF[pts[i + 1]][1]};
-    vector<vector<double>> edge;
-    edge.push_back(p0);
-    edge.push_back(p1);
-    edges.push_back(edge);
-  }
-  RectRegion pic0 = RectRegion(edges, "pic");
-  pic0.MeshGen(1, nBoxLeft);
-  pic0.Tec360Pts("pic0.dat");
-  combinedReg.AddRegion(pic0);
-  // right
-  pts = {2, 3, 11, 10, 2};
-  edges.clear();
-  for (int i = 0; i < 4; ++i) {
-    vector<double> p0 = {g_ptsF[pts[i]][0], g_ptsF[pts[i]][1]};
-    vector<double> p1 = {g_ptsF[pts[i + 1]][0], g_ptsF[pts[i + 1]][1]};
-    vector<vector<double>> edge;
-    edge.push_back(p0);
-    edge.push_back(p1);
-    edges.push_back(edge);
-  }
-  RectRegion pic1 = RectRegion(edges, "pic");
-  pic1.MeshGen(1, nBoxLeft);
-  pic1.Tec360Pts("pic1.dat");
-  combinedReg.AddRegion(pic1);
-  // upper
-  pts = {6, 7, 10, 9, 6};
-  edges.clear();
-  for (int i = 0; i < 4; ++i) {
-    vector<double> p0 = {g_ptsF[pts[i]][0], g_ptsF[pts[i]][1]};
-    vector<double> p1 = {g_ptsF[pts[i + 1]][0], g_ptsF[pts[i + 1]][1]};
-    vector<vector<double>> edge;
-    edge.push_back(p0);
-    edge.push_back(p1);
-    edges.push_back(edge);
-  }
-  RectRegion pic2 = RectRegion(edges, "pic");
-  pic2.MeshGen(nBoxDown - 2, 1);
-  pic2.Tec360Pts("pic2.dat");
-  combinedReg.AddRegion(pic2);
-  // lower
-  pts = {1, 2, 5, 4, 1};
-  edges.clear();
-  for (int i = 0; i < 4; ++i) {
-    vector<double> p0 = {g_ptsF[pts[i]][0], g_ptsF[pts[i]][1]};
-    vector<double> p1 = {g_ptsF[pts[i + 1]][0], g_ptsF[pts[i + 1]][1]};
-    vector<vector<double>> edge;
-    edge.push_back(p0);
-    edge.push_back(p1);
-    edges.push_back(edge);
-  }
-  RectRegion pic3 = RectRegion(edges, "pic");
-  pic3.MeshGen(nBoxDown - 2, 1);
-  pic3.Tec360Pts("pic3.dat");
-  combinedReg.AddRegion(pic3);
+  setRadiusMesh(2.*M_PI*radiusFar/Nfar, 1.1, 4.*M_PI*radiusFar/Nfar);
+  std::vector<void *> edges0;
+  void *edgetmp;
+  // edge 11-0
+  edges0.push_back((void *)edge4);
+  edges0.push_back((void *)radiusEdge);
+  edges0.push_back(edgetmp);
+  edges0.push_back(edgetmp);
+  RectRegion Rect(RectRegion(edges0, "cyl4", false));
+  setRadiusLayers(1);
+  Rect.MeshGen(Nfar, 1, eBoundaryLayer1);
+  Rect.Tec360Pts("cyl4.dat");
+  combinedReg.AddRegion(Rect);
   return 0;
 }
 
@@ -313,25 +180,10 @@ int outputXML(MeshRegions &combinedReg) {
   vector<int> comp3;
   comp3.push_back(0);
   // wall
-  combinedReg.defineBoundary((void *)edge45, Cedge45.m_N, 0, curvedpts);
-  combinedReg.defineBoundary((void *)edge56, Cedge56.m_N, 0, curvedpts);
-  combinedReg.defineBoundary((void *)edge1011, Cedge1011.m_N, 0, curvedpts);
-  combinedReg.defineBoundary((void *)edge110, Cedge110.m_N, 0, curvedpts);
-  combinedReg.defineBoundary((void *)edge01, Cedge01.m_N, 5);
-  combinedReg.defineBoundary((void *)edge12, Cedge12.m_N, 5);
-  combinedReg.defineBoundary((void *)edge23, Cedge23.m_N, 5);
-  combinedReg.defineBoundary((void *)edge34, Cedge34.m_N, 5);
-  combinedReg.defineBoundary((void *)edge67, Cedge67.m_N, 6);
-  combinedReg.defineBoundary((void *)edge78, Cedge78.m_N, 6);
-  combinedReg.defineBoundary((void *)edge89, Cedge89.m_N, 6);
-  combinedReg.defineBoundary((void *)edge910, Cedge910.m_N, 6);
-  // inlet
-  combinedReg.defineBoundary((void *)far08, Cfar08.m_N, 1);
-  // outlet
-  combinedReg.defineBoundary((void *)far311, Cfar311.m_N, 2);
-  // side
-  combinedReg.defineBoundary((void *)far03, Cfar03.m_N, 3);
-  combinedReg.defineBoundary((void *)far811, Cfar811.m_N, 4);
+  combinedReg.defineBoundary((void *)edge0, Ncylinder, 0, curvedpts);
+  combinedReg.defineBoundary((void *)edge1, Ncylinder, 1, curvedpts);
+  combinedReg.defineBoundary((void *)edge2, Ncylinder, 2, curvedpts);
+  combinedReg.defineBoundary((void *)edge4, Nfar, 3);
   // output
   combinedReg.outXml("outerRegion.xml");
   combinedReg.outCOMPO("outerRegion.xml", comp3);
