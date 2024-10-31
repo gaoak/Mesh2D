b0054d8968f6fd53db553f7db25857c6e233a8f7
Ankang Gao <ankanggao@ustc.edu.cn>
2024-10-31 14:40:03 +0800

    optimize code

--- edgefunctions.h
+++ edgefunctions.h
@@ -3,21 +3,17 @@
 #define NUMPTS 20
 double hcenter = 0.02;
 double hgrowth = 1.4;
-double hfar = 0.1;
+double hfar = 0.2;
 
 double p0x = 0.;
-//double p1x = -1.5;
-//double p2x = 1.5;
-double p1x = 6*M_PI;
+double p1x = 8.*M_PI;
 
 double p0y = -1;
 double p1y = -0.7;
 double p2y = 0.7;
 double p3y = 1.;
 
-//int N0x = findNlayers(hcenter, hgrowth, -p0x, 3*hfar);
-int N1x = std::ceil((p1x - p0x)/(3*hfar));
-//int N2x = findNlayers(hcenter, hgrowth, p3x, 3*hfar);
+int N1x = std::ceil((p1x - p0x)/hfar);
 
 double pts[NUMPTS][2];
 
@@ -25,42 +21,21 @@ int N0y = findNlayers(hcenter, hgrowth, p1y-p0y, hfar);
 int N1y = std::ceil((p2y-p1y)/(hfar));
 int N2y = findNlayers(hcenter, hgrowth, p3y-p2y, hfar);
 
-//CompositEdge Cedge03;
-//CompositEdge Cedge69;
 CompositEdge Cedge06;
 CompositEdge Cedge17;
 
 LineEdge Cedge01(pts[0], pts[1], N1x, UNIFORM, 0., 0.);
-//LineEdge Cedge12(pts[1], pts[2], N1x, UNIFORM, 0., 0.);
-//LineEdge Cedge23(pts[2], pts[3], N1x, UNIFORM, 0., 0.);
 std::vector<double> edge01(double s) {
     return Cedge01.Evaluate(s);
 }
-//std::vector<double> edge12(double s) {
-//    return Cedge12.Evaluate(s);
-//}
-//std::vector<double> edge23(double s) {
- //   return Cedge23.Evaluate(s);
-//}
 std::vector<double> edge17(double s) {
     return Cedge17.Evaluate(s);
 }
 
 LineEdge Cedge67(pts[6], pts[7], N1x, UNIFORM, 0., 0.);
-//LineEdge Cedge78(pts[7], pts[8], N1x, UNIFORM, 0., 0.);
-//LineEdge Cedge89(pts[8], pts[9], N1x, UNIFORM, 0., 0.);
 std::vector<double> edge67(double s) {
     return Cedge67.Evaluate(s);
 }
-//std::vector<double> edge78(double s) {
-//    return Cedge78.Evaluate(s);
-//}
-//std::vector<double> edge89(double s) {
-//    return Cedge89.Evaluate(s);
-//}
-//std::vector<double> edge69(double s) {
-//    return Cedge69.Evaluate(s);
-//}
 
 LineEdge Cedge010(pts[0], pts[10], N0y, BOUNDARYLAYER0, hcenter, hgrowth, N0y-5, 0, 0, 0);
 LineEdge Cedge104(pts[10], pts[4], N1y, UNIFORM, 0, 0);
@@ -90,19 +65,12 @@ std::vector<double> edge115(double s) {
 std::vector<double> edge57(double s) {
     return Cedge57.Evaluate(s);
 }
-//std::vector<double> edge39(double s) {
-//    return Cedge39.Evaluate(s);
-//}
 
 int InitPts(){
     pts[0][0] = p0x;
     pts[0][1] = p0y;
     pts[1][0] = p1x;
     pts[1][1] = p0y;
-    //pts[2][0] = p2x;
-    //pts[2][1] = p0y;
-    //pts[3][0] = p1x;
-    //pts[3][1] = p0y;
 
     pts[10][0] = p0x;
     pts[10][1] = p1y;
@@ -118,18 +86,6 @@ int InitPts(){
     pts[6][1] = p3y;
     pts[7][0] = p1x;
     pts[7][1] = p3y;
-    //pts[8][0] = p2x;
-    //pts[8][1] = p3y;
-    //pts[9][0] = p3x;
-    //pts[9][1] = p3y;
-
-    //Cedge03.addEdge(Cedge01, (void*) edge01);
-   // Cedge03.addEdge(Cedge12, (void*) edge12);
-    //Cedge03.addEdge(Cedge23, (void*) edge23);
-
-    //Cedge69.addEdge(Cedge67, (void*) edge67);
-    //Cedge69.addEdge(Cedge78, (void*) edge78);
-    //Cedge69.addEdge(Cedge89, (void*) edge89);
 
     Cedge06.addEdge(Cedge010, (void*) edge010);
     Cedge06.addEdge(Cedge104, (void*) edge104);
