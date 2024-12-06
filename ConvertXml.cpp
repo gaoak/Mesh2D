31f9f57410e2f4e7250af0cc281d3a3db2d45991
Ankang Gao <ankanggao@ustc.edu.cn>
2024-12-06 09:20:55 +0800

    split boundary

--- ConvertXml.cpp
+++ ConvertXml.cpp
@@ -12,6 +12,26 @@
 #include <vector>
 using namespace std;
 
+bool leftbnd(vector<double>&p) {
+  return p[0] < -10. && fabs(p[1])<5.;
+}
+
+bool rightbnd(vector<double>&p) {
+  return p[0] > 10. && fabs(p[1])<5.;
+}
+
+bool upbnd(vector<double>&p) {
+  return p[1] > 10. && fabs(p[0])<5.;
+}
+
+bool downbnd(vector<double>&p) {
+  return p[1] < -10. && fabs(p[0])<5.;
+}
+
+bool wallbnd(vector<double>&p) {
+  return fabs(p[0]) < 3. && fabs(p[1])<3.;
+}
+
 int main(int argc, char *argv[]) {
   bool merge = false;
   std::string infilename, outfilename;
@@ -26,6 +46,13 @@ int main(int argc, char *argv[]) {
   cout << "start meshing --------" << endl;
   MeshRegions region("Rxmk:", 1.E-6);
   region.loadFromXml(infilename, "");
+  std::map<int, void *> edgeFuns;
+  edgeFuns[0] = (void*) wallbnd;
+  edgeFuns[1] = (void*) leftbnd;
+  edgeFuns[2] = (void*) rightbnd;
+  edgeFuns[3] = (void*) upbnd;
+  edgeFuns[4] = (void*) downbnd;
+  region.defineBoundary(edgeFuns, 0.9*M_PI/2.);
   vector<int> comp3;
   comp3.push_back(0);
   region.outXml(outfilename);
